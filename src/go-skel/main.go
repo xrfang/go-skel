b098dea1a0515374fbef0cccc4f8e442f1f71856
xrfang <xrfang@gmail.com>
2017-05-27 08:46:33 +0800

    Added verinfo() support

--- src/go-skel/main.go
+++ src/go-skel/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 )
 
 func main() {
+	ver := flag.Bool("version", false, "show version info")
+	flag.Parse()
+	if *ver {
+		fmt.Println(verinfo())
+		return
+	}
 	fmt.Println("This is [main] project.")
 }
